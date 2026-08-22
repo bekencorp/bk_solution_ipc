af83a04f681c90091c5b343a9fb59fb8c3793904
itester <itester@aclsemi.com>
2026-08-22 23:27:08 +0800

    [CI] [100227]  <multimedia> <fix> improve H264 encoder rate control handling

--- components/smart_lock/src/doorbell_devices.c
+++ components/smart_lock/src/doorbell_devices.c
@@ -80,10 +80,10 @@ static bk_err_t doorbell_apply_h264_qp_preset(void *encode_handle)
 #elif CONFIG_H264_QP_PRESET_BALANCED
     preset_name = "balanced";
     rate_ctrl.bitrate = 1500000;
-    rate_ctrl.qp_min_i = 20;
-    rate_ctrl.qp_max_i = 51;
-    rate_ctrl.qp_min_p = 26;
-    rate_ctrl.qp_max_p = 51;
+    rate_ctrl.qp_min_i = 26;
+    rate_ctrl.qp_max_i = 43;
+    rate_ctrl.qp_min_p = 34;
+    rate_ctrl.qp_max_p = 43;
 #elif CONFIG_H264_QP_PRESET_ANTI_STUTTER
     preset_name = "anti-stutter";
     rate_ctrl.bitrate = 1200000;
